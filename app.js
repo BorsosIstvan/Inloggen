72eb58cee86c6f7928618a419b4d9c86aaf12e9a
BorsosIstvan <bistvanus@gmail.com>
2024-04-07 10:40:54 +0200

    hoi

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/templates/index.html');
 });
 
+// Route for the login page
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/templates/login.html');
+});
+
+// Route for the signup page
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname + '/templates/signup.html');
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
